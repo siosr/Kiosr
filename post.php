bbd3fbc67de11f28122006b2a655a6bd36cba725
Kiosr <51766320+siosr@users.noreply.github.com>
2019-06-16 12:23:27 +0800

    Update post.php

--- post.php
+++ post.php
@@ -6,11 +6,9 @@
 	$this->need('h.php'); ?>
 <small><?php echo $this->date('d M Y').' 字数统计:'.art_count($this->cid).' Hits:'.get_post_view($this) ?></small>
 <h1 id="post"><?php $this->title() ?></h1>
-<p class="view">by <a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a><a style="float:right" class="link" href="<?php echo $this->options->siteUrl().$this->cid ?>.md" target="_blank">查看本文Markdown版本</a></p>
-<article>
-	<?php $this->content(); ?>
-</article>
-<p>tag:<?php $this->tags('', true, 'none'); ?></p>
+<p class="view"><a href="<?php $this->author->permalink(); ?>"><i class="i user"></i><?php $this->author(); ?></a> | <a href="<?php echo $this->options->siteUrl().$this->cid ?>.md" target="_blank"><i class="i md" ></i>Markdown</a></p>
+<article><?php $this->content(); ?></article>
+<p><i class="i tag"></i><?php $this->tags('', true, 'none'); ?></p>
 <?php $this->thePrev('上一篇 : %s',''); ?><br>
-<p><?php $this->theNext('下一篇 : %s',''); ?></p>
+<?php $this->theNext('下一篇 : %s',''); ?>
 <?php $this->need('f.php');} ?>
