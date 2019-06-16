bc70760a3724f2bde5ff36d977c812ae84f67d61
Kiosr <51766320+siosr@users.noreply.github.com>
2019-06-16 12:32:19 +0800

    Update h.php

--- h.php
+++ h.php
@@ -7,14 +7,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<script>var $caches="<?php echo themeurl('/caches/cache.json')?>",js={css:function(a){var b,c;if(!a||0===a.length)throw new Error('argument "path" is required !');b=document.getElementsByTagName("head")[0],c=document.createElement("link"),c.href=a,c.rel="stylesheet",c.type="text/css",b.appendChild(c)},js:function(a){var b,c;if(!a||0===a.length)throw new Error('argument "path" is required !');b=document.getElementsByTagName("head")[0],c=document.createElement("script"),c.src=a,c.type="text/javascript",b.appendChild(c)}};js.css("<?php echo themeurl('/main.css')?>?v=61"),js.js("<?php echo themeurl('/main.js')?>?v=57");</script>
+	<script>var $caches="<?php echo themeurl('/caches/cache.json')?>",js={css:function(a){var b,c;if(!a||0===a.length)throw new Error('argument "path" is required !');b=document.getElementsByTagName("head")[0],c=document.createElement("link"),c.href=a,c.rel="stylesheet",c.type="text/css",b.appendChild(c)},js:function(a){var b,c;if(!a||0===a.length)throw new Error('argument "path" is required !');b=document.getElementsByTagName("head")[0],c=document.createElement("script"),c.src=a,c.type="text/javascript",b.appendChild(c)}};js.css("<?php echo themeurl('/main.css')?>?v=77"),js.js("<?php echo themeurl('/main.js')?>?v=63");</script>
 	</head>
 	<body>
 		<div id="wrap">
 			<div id="backdrop"></div>
 			<header id="bar" class="hidden">
 				<h1><a nohover href="<?php $this->options->siteUrl(); ?>"><?php $this->options->t(); ?><small><?php $this->options->s(); ?></small></a></h1>
-				<img src="<?php echo themeurl('/1.jpg')?>" data-src="<?php $this->options->logo() ?>" >
+				<img src="<?php echo img()?>" data-src="<?php $this->options->logo() ?>" >
 				<?php $this->options->dh() ?>
 			</header>
 			<div id="toggle"><span></span><span></span><span></span></div>
