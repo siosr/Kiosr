8a1a71230422f15239f35e07b3e310cf894f3d61
Kiosr <51766320+siosr@users.noreply.github.com>
2019-07-07 14:15:37 +0800

    6.1
    去除了鸡肋功能,只留下日期、标签、评论数

--- post.php
+++ post.php
@@ -4,11 +4,8 @@
 }else{
 	if(strpos($_SERVER["PHP_SELF"],"themes")) header('Location:/');
 	$this->need('h.php'); ?>
-<small><?php echo $this->date('d M Y').' 字数统计:'.art_count($this->cid).' Hits:'.get_post_view($this) ?></small>
-<h1 id="post"><?php $this->title() ?></h1>
-<p class="view"><a href="<?php $this->author->permalink(); ?>"><i class="i user"></i><?php $this->author(); ?></a> | <a href="<?php echo $this->options->siteUrl().$this->cid ?>.md" target="_blank"><i class="i md" ></i>Markdown</a></p>
+<a><h1 id="post"><?php $this->title() ?></h1></a>
+<p><a><?php $this->date('M d,Y')?></a></p>
 <article><?php $this->content(); ?></article>
-<p><i class="i tag"></i><?php $this->tags('', true, 'none'); ?></p>
-<?php $this->thePrev('上一篇 : %s',''); ?><br>
-<?php $this->theNext('下一篇 : %s',''); ?>
+<p class="tag-a"><i class="i tag"></i><?php $this->tags('', true, 'none'); ?><a id="response"><i class="i reply"></i><span id="pls"><?php $this->commentsNum(_t('0'), _t('1'), _t('%d')); ?></span>&nbsp;</a></p>
 <?php $this->need('f.php');} ?>
