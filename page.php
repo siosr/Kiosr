a4dc0b04dc7d90421347e52ce369bd4903288b3f
Kiosr <51766320+siosr@users.noreply.github.com>
2019-07-07 14:14:04 +0800

    6.1
    这里好像没什么变化

--- page.php
+++ page.php
@@ -1,7 +1,5 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('h.php'); ?>
-<h1><?php $this->title() ?></h1>
-<article>
-	<?php $this->content(); ?>
-</article>
+<h1 id="page"><?php $this->title() ?></h1>
+<article><?php $this->content(); ?></article>
 <?php $this->need('f.php'); ?>
