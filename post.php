f9500853474bd98f4b3b3a8eab63c134dd61d477
Kiosr <51766320+siosr@users.noreply.github.com>
2019-07-19 14:26:35 +0800

    6.2🚗

--- post.php
+++ post.php
@@ -5,7 +5,7 @@
 }else{
 	if(strpos($_SERVER["PHP_SELF"],"themes")) header('Location:/');
 	$this->need('h.php'); ?>
-<a><h1 id="post"><?php $this->title() ?></h1></a>
+<h1 id="post"><?php $this->title() ?></h1>
 <p><a><?php $this->date('M d,Y')?></a></p>
 <article><?php $this->content(); ?></article>
 <p class="tag-a"><i class="i tag"></i><?php $this->tags('', true, 'none'); ?><?php if($this->allow('comment')): ?><a id="response"><i class="i reply"></i><span id="pls"><?php $this->commentsNum(_t('0'), _t('1'), _t('%d')); ?></span>&nbsp;</a><?php endif; ?></p>
