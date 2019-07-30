e01d51037e3e999bcdd4bbe1b767acd867f9f6ab
Kiosr <51766320+siosr@users.noreply.github.com>
2019-07-30 16:23:48 +0800

    6.3.1 更新导航目录

--- h.php
+++ h.php
@@ -8,8 +8,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <script>var $caches="<?php echo themeurl('/caches/cache.json')?>";</script>
-	<link async href="<?php echo themeurl('/main.css')?>?v=272" rel="stylesheet" type="text/css">
-	<script async src="<?php echo themeurl('/main.js')?>?v=318"></script>
+	<link async href="<?php echo themeurl('/main.css')?>?v=273" rel="stylesheet" type="text/css">
+	<script async src="<?php echo themeurl('/main.js')?>?v=325"></script>
 	</head>
 	<body>
 		<div id="wrap">
@@ -19,13 +19,12 @@
 			</div>
 			<header id="bar" class="hidden">
 				<h1><a nohover href="<?php $this->options->siteUrl(); ?>"><?php $this->options->t(); ?><small><?php $this->options->s(); ?></small></a></h1>
-				<?php $this->options->dh() ?>
-				<div class="nav menu"><?php getExplode($this->options->fl);?></div>
+				<div class="nav menu"><?php m($this->options->dh);?><a id="s">搜索</a></div>
 			</header>
 			<section id="search">
 				<form id="search-form" method="post" action="./" role="search">
 					<input id="search-input" type="text" name="s" class="ins-search-input" placeholder="想要查找什么...">
-					<b id="search-close">×</b>
+					<b id="search-close"></b>
 				</form>
 				<div id="search-result"></div>
 			</section><a id="Ty" href="#"></a>
